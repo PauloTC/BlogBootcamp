59792123b02fea58ebf2849aede30ec4bb3f4eb9
Paulo Tejada <paulo@MacBook-Pro-de-Paulo.local>
2019-06-26 22:04:54 -0500

    base whitout styles

---
+++ src/pages/contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+
+
+const ContactPage = () => {
+    return (
+        <Layout>
+            <h1>Contact</h1>
+            <p>Here is the contact Page contact to my  <a  href="https://twitter.com/backertend">twitter</a></p>
+        </Layout>
+    )
+}
+
+export default ContactPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,17 @@
 import React from "react"
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+
+    return(
+        <Layout>
+            <h1>Hello</h1>
+            <h3> Soy Paulo y vivo en Peru  </h3>
+            <p>Need a Developer ? <Link  to="/contact" > Contact Me </Link> </p>
+        </Layout>
+
+    )
+}
+
+export default IndexPage
